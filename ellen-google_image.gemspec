6be032736dc9eff450f18bd94b42c921d5a6666b
Ryo Nakamura <r7kamura@gmail.com>
2014-04-20 18:56:31 +0900

    Adapt to ellen 0.0.8's new handler interface

--- ellen-google_image.gemspec
+++ ellen-google_image.gemspec
@@ -16,7 +16,7 @@ Gem::Specification.new do |spec|
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
 
-  spec.add_dependency "ellen", ">= 0.0.5"
+  spec.add_dependency "ellen", ">= 0.0.8"
   spec.add_dependency "faraday"
   spec.add_dependency "faraday_middleware"
   spec.add_development_dependency "bundler", "~> 1.6"
--- lib/ellen/handlers/google_image.rb
+++ lib/ellen/handlers/google_image.rb
@@ -1,16 +1,26 @@
 module Ellen
   module Handlers
     class GoogleImage < Base
-      on /image (.+)/, description: "Search image from Google" do |message|
-        if url = Ellen::GoogleImage::Client.new(query: message[1]).get
-          say url
+      on /image (.+)/, name: "image", description: "Search image from Google"
+
+      on /animate (.+)/, name: "animate", description: "Search animation from Google"
+
+      def image(message)
+        if url = search(message[1])
+          robot.say url
         end
       end
 
-      on /animate (.+)/, description: "Search animation from Google" do |message|
-        if url = Ellen::GoogleImage::Client.new(query: message[1], animated: true).get
-          say url
+      def animate(message)
+        if url = search(message[1], animated: true)
+          robot.say url
         end
+      end
+
+      private
+
+      def search(query, options = {})
+        Ellen::GoogleImage::Client.new(options.merge(query: query)).get
       end
     end
   end
